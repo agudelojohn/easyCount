8efcade380071aa93fd2e80f4759302020011a54
John Agudelo <jhbr08@hotmail.com>
2022-05-19 23:21:50 -0500

    Navbar added and working with the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,59 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 //Components
 import { RecipesList } from "./components/recipes/RecipesList";
-import { Home } from './components/home/Home';
+import { Home } from "./components/home/Home";
 
 export function App() {
+  const smallRightMargin = {
+    marginRight: "15px",
+  };
   return (
     <Router>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Easy Count</Navbar.Brand>
+          <Nav className="me-auto">
+            <NavLink
+              to="/"
+              className="btn btn-light "
+              style={smallRightMargin}
+              activeClassName="active"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/recipesList"
+              className="btn btn-light"
+              style={smallRightMargin}
+              activeClassName="active"
+            >
+              Recieps list
+            </NavLink>
+            {/* <Link to='/recipesList' className='btn btn-success'>
+                      New recipe
+                  </Link> */}
+            {/* <Link to='/recipesList' className='btn btn-success'>
+                      Total ingredients
+                  </Link> */}
+          </Nav>
+        </Container>
+      </Navbar>
       <Container>
         <Routes>
-          <Route path="/recipesList" element={<RecipesList/>} />
-          <Route path="/" element={<Home/>} />
+          <Route path="/recipesList" element={<RecipesList />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Container>
     </Router>
   );
 }
+
+//TODO: Add components missing
