e0ab1e6594dba09bf5ec113340c03367c76d2136
John Agudelo <jhbr08@hotmail.com>
2022-06-01 18:48:37 -0500

    New rendering way, using last React version

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
 );
-<App />;
