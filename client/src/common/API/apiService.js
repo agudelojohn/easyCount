7eeff7d897894e5b81e389ae38709b79e04faa73
John Agudelo <jhbr08@hotmail.com>
2022-06-15 19:34:23 -0500

    API endpoint using Mockup API

--- client/src/common/API/apiService.js
+++ client/src/common/API/apiService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseUrl = "https://8da0iso4rc.execute-api.us-east-1.amazonaws.com/dev_1";
+// const baseUrl = "https://8da0iso4rc.execute-api.us-east-1.amazonaws.com/dev_1";
+const baseUrl = 'https://62aa67e0371180affbd4d8a4.mockapi.io/mockpuapi/';
+
 
 // const setConfiguration = (method, url, dataRequest) => {
 //   return {
